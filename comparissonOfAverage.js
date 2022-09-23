22721891e9c09a4c585ff41ad049d24b4d804d58
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-23 03:08:44 +0300

    34

---
+++ comparissonOfAverage.js
@@ -0,0 +1,28 @@
+/*
+
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array
+
+*/
+
+function betterThanAverage(classPoints, yourPoints) {
+  
+    const summary = classPoints.reduce((add, sum) => {
+      return add + sum;
+    }, 0);
+    
+    const avg = summary / classPoints.length;
+    
+    if (yourPoints > avg) {
+      return true;
+    } else {
+      return false;
+    }
+}
