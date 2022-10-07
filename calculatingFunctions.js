4a027f845e7b98293fe7acb302b5f6a683fa0614
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-08 01:18:01 +0300

    55

--- calculatingFunctions.js
+++ calculatingFunctions.js
@@ -31,4 +31,4 @@ function nine(func)   { return func ? func(9) : 9; };
 function plus( b )      { return function( a ) { return a + b; }; };
 function minus( b )     { return function( a ) { return a - b; }; };
 function times( b )     { return function( a ) { return a * b; }; };
-function dividedBy( b ) { return function( a ) { return a / b; }; };
+function dividedBy( b ) { return function( a ) { return Math.floor( a / b ); }; };
