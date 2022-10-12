f6621bbd5eb41d0e58ee9db0d620165e47277a48
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-12 16:10:31 +0300

    443

---
+++ digitsOfNumberSum.js
@@ -0,0 +1,21 @@
+/*
+
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+
+*/
+
+function sumDigits(number) {
+    let myArray = Math.abs(number).toString().split('')
+    let product = 0;
+    
+    for(let i = 0; i < myArray.length; i++)
+      product += +myArray[i]
+    return product;
+}
